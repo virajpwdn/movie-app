910eeca8f6e9d7b0c2608fbc54b182e35201460a
viraj <virajpatwardhan6@gmail.com>
2026-01-30 12:05:01 +0530

    feat: scrollbar hidden

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,11 +1,19 @@
-import { Link } from "expo-router";
-import { Text, View } from "react-native";
+import { icons } from "@/constants/icons";
+import { images } from "@/constants/images";
+import { Image, ScrollView, View } from "react-native";
 
 export default function Index() {
   return (
-    <View className="flex-1 justify-center items-center">
-      <Text className="text-5xl text-accent">Welcome</Text>
-      <Link href="/onboarding">Onboarding</Link>
+    <View className="bg-primary flex-1">
+      <Image source={images.bg} className="absolute w-full z-0" />
+
+      <ScrollView
+        className="flex-1 px-5"
+        showsVerticalScrollIndicator={false} //To hide scrollbar
+        contentContainerStyle={{ minHeight: "100%", paddingBottom: 10 }} //Style for inner container
+      >
+        <Image source={icons.logo} className="w-12 h-10 mt-20 mb-5 mx-auto" />
+      </ScrollView>
     </View>
   );
 }
